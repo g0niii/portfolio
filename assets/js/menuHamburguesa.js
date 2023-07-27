86a5e02d4759c6a108750026e8a9c770bf642e24
g0niii <gonilovemor@gmail.com>
2023-07-27 13:41:33 -0300

    13:41-27/7

--- assets/js/menuHamburguesa.js
+++ assets/js/menuHamburguesa.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     let button = document.querySelector(".wrapper__menutoggle");
+    let icon_bars = document.querySelector(".wrapper__menutoggle .fa-bars");
+    let icon_xmark = document.querySelector(".wrapper__menutoggle .fa-xmark");
     let aside = document.querySelector(".wrapper__aside");
   
     button.addEventListener("click", (event) => {
       let isVisible = aside.classList.contains("wrapper__aside--visible");
       if (!isVisible) {
         aside.classList.add("wrapper__aside--visible");
+        icon_bars.style.opacity=0;
+        icon_xmark.style.opacity=1;
       } else {
         aside.classList.remove("wrapper__aside--visible");
+        icon_bars.style.opacity=1;
+        icon_xmark.style.opacity=0;
       }
     });
   });
+
+
